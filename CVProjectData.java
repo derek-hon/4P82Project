97df4c1d74b56ecb80b26acaa052b0dc27ab052f
Derek Hon <derek-hon@github.com>
2019-12-18 21:17:29 -0500

    Some liscence comments added. Not finished

--- CVProjectData.java
+++ CVProjectData.java
@@ -1,3 +1,10 @@
+/*
+  Copyright 2006 by Sean Luke
+  Licensed under the Academic Free License version 3.0
+  See the file "LICENSE" for more information
+*/
+
+
 package ec.Project4P82;
 
 import ec.gp.*;
